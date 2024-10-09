d31a32e975b4b5720e36c028f20e8df7070e00ae
Elia Trachsel <trachsele@tikgpu02.ee.ethz.ch>
2024-10-09 12:17:21 +0200

    push test

--- ultra/datasets.py
+++ ultra/datasets.py
@@ -7,6 +7,7 @@ from torch_geometric.datasets import RelLinkPredDataset, WordNet18RR
 
 from ultra.tasks import build_relation_graph
 
+#this is a test for pushing to git
 
 class GrailInductiveDataset(InMemoryDataset):
 
